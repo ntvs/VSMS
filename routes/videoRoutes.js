2089540ad9519aa80360b9ad5a881411459f1786
ntvs <NickRibs@hotmail.com>
2023-04-10 19:05:20 -0400

    Added file route boilerplate

--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -49,6 +49,33 @@ router.get("/file", (req, res) => {
     });
 });
 
+router.get("/file/:videoId", async (req, res) => {
+
+    //If the provided ID is not a valid ObjectId, return an error
+    if (!mongoose.Types.ObjectId.isValid(req.params.videoId)) {
+        return res.status(400).send({
+            "error": "Invalid video ID!"
+        });
+    }
+
+    //Check if a video exists with the provided ID
+    let video = await Video.findById(req.params.videoId);
+
+    //If the video does not exist, return an error
+    if (!video) {
+        return res.status(400).send({
+            "error": "A video with that ID does not exist!"
+        });
+    }
+
+    //Stream video file
+    
+    return res.status(200).send({
+        "msg": "OK"
+    });
+
+});
+
 //-------------------------------------------------------------------
 
 module.exports = router;
